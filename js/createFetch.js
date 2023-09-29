c45e9c6534de1bd784bb28248dcb0068493eb6c9
Leandro Souza <leandrobonfimdesouza10@gmail.com>
2023-09-29 16:19:22 -0300

    Continuação nas alterações de como fazer requisições AJAX

--- js/createFetch.js
+++ js/createFetch.js
@@ -8,13 +8,22 @@ export function createFetch(method, url, data = null){
         return response;
     }
 
-    return fetch(url, {
-        method,
-        body: JSON.stringify({data}),
-        headers: {
-            "Content-type": "application/json;charset=UTF-8"
+
+   function returnFetch(){
+        if(method == "GET"){
+            return fetch(url)
+        }else{
+            return fetch(url, {
+                method,
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-type": "application/json;charset=UTF-8"
+                }
+            })
         }
-    })
+   } 
+    
+   return returnFetch()
         .then(response => handleError(response))
         .then(response => response.json())
         .then(response => response);
