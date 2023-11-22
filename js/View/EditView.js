f16497fd6c8a412df542d3a5d230892e607bb65f
Leandro Souza <leandrobonfimdesouza10@gmail.com>
2023-11-22 12:28:20 -0300

    Continução da criação do EditView

--- js/View/EditView.js
+++ js/View/EditView.js
@@ -34,7 +34,7 @@ export class EditView{
                         $li.appendChild($btnEditPersonagem);
                         this.$resultados.appendChild($li);
 
-                        // this.apagarPersonagens();
+                        this.btnEditarPersonagens();
                     })
                 })
         }else{
@@ -52,7 +52,7 @@ export class EditView{
                 $li.appendChild($btnEditPersonagem);
                 this.$resultados.appendChild($li);
 
-                // this.apagarPersonagens();
+                this.btnEditarPersonagens();
             })
         }
     } 
@@ -118,23 +118,126 @@ export class EditView{
         })
     }
 
-    editarPersonagens(){
-        let $inputsTextArea = document.querySelectorAll('textarea');
-        let dataArray = [];
-        $inputsTextArea.forEach(input, ()=>{
-                if(input.value === ""){
-                    alert("Preencha todos os campos");
-                    return;
-                }
-
-                dataArray.push(input.value);               
+    btnEditarPersonagens(){
+        let $btnEdit = document.querySelectorAll('.editPersona');
+        $btnEdit.forEach(btn => {
+            btn.addEventListener('click', (e)=> {
+                this.telaEditarPersonagens(e);
+            })
         })
+    }
 
-        let _dataArray = {"nivel": dataArray[0], "personagem": dataArray[1], "dicaUm": dataArray[2], "dicaDois": dataArray[3], "dicaTres": dataArray[4], "dicaQuatro": dataArray[5], "dicaCinco": dataArray[6], "dicaSeis": dataArray[7], "dicaSete": dataArray[8], "dicaOito": dataArray[9], "dicaNove": dataArray[10], "dicaDez": dataArray[11], "id": el[12]};
-                            console.log(_valores);
+    telaEditarPersonagens(e){
+        let personagemEditar = e.target.getAttribute("value");
+        personagemEditar = parseFloat(personagemEditar);
+        this.jogoController.lista()
+            .then(resposta => {
+                resposta.forEach(personagem => {
+                    console.log(personagem.id, personagemEditar);
+                    if(personagem.id == personagemEditar){
+                        console.log(personagem.nivel)
+                        let levelPergunta = personagem.nivel;
+                        levelPergunta = levelPergunta.toLowerCase();
+                        let _level = undefined;
 
-        this.jogoController.edit(`http://localhost:3000/perguntas/${_dataArray.id}`, _valores)
-                .then(response => alert("Deu certo"));
+                        switch(levelPergunta){
+                            case "fácil":
+                                _level = "easy";
+                                break;
+                            case "médio":
+                                _level = "medium";
+                                break;
+                            case "difícil":
+                                _level = "hard";
+                                break;
+                            default:
+                                alert("Alguma coisa deu errado com o nível da pergunta");  
+                        }
 
+                        this.$resultados.innerHTML = ` 
+                        <div class="container caixaPerguntas">
+                        <div class="dificuldade ${_level}"><textarea>${personagem.nivel}</textarea></div>
+                        <span class="CodigoDaPergunta h1 mr-3">0${personagem.id}</span><span class="h1 personagem">Personagem:</span><span class="h2 p-2"><textarea>${personagem.personagem}</textarea></span>
+                        <div class="dicasPersonagens">
+                            <div class="metade1">
+                                <div class="divPai">
+                                    <div class="divDicas"><span class="mr-2 ml-1 number">1</span><span class="dica"><textarea>${personagem.dicaUm}</textarea></span></div>
+                                </div>
+                                <br>
+                                <div class="divPai">
+                                    <div class="divDicas"><span class="mr-2 ml-1 number">2</span><span class="dica"><textarea>${personagem.dicaDois}</textarea></span></div>
+                                </div>
+                                <br>
+                                <div class="divPai">
+                                    <div class="divDicas"><span class="mr-2 ml-1 number">3</span><span class="dica"><textarea>${personagem.dicaTres}</textarea></span></div>
+                                </div>
+                                <br>
+                                <div class="divPai">
+                                    <div class="divDicas"><span class="mr-2 ml-1 number">4</span><span class="dica"><textarea>${personagem.dicaQuatro}</textarea></span></div>
+                                </div>
+                                <br>
+                                <div class="divPai">
+                                    <div class="divDicas"><span class="mr-2 ml-1 number">5</span><span class="dica"><textarea>${personagem.dicaCinco}</textarea></span></div>
+                                </div>
+                                <br>
+                            </div>
+                            
+                            <div class="metade2">
+                                <div class="divPai">
+                                    <div class="divDicas"><span class="mr-2 ml-1 number">6</span><span class="dica"><textarea>${personagem.dicaSeis}</textarea></span></div>
+                                </div>
+                                <br>
+                                <div class="divPai">
+                                    <div class="divDicas"><span class="mr-2 ml-1 number">7</span><span class="dica"><textarea>${personagem.dicaSete}</textarea></span></div>
+                                </div>
+                                <br>
+                                <div class="divPai">
+                                    <div class="divDicas"><span class="mr-2 ml-1 number">8</span><span class="dica"><textarea>${personagem.dicaOito}</textarea></span></div>
+                                </div>
+                                <br>
+                                <div class="divPai">
+                                    <div class="divDicas"><span class="mr-2 ml-1 number">9</span><span class="dica"><textarea>${personagem.dicaNove}</textarea></span></div>
+                                </div>
+                                <br>
+                                <div class="divPai">
+                                    <div class="divDicas"><span class="mr-2 ml-1 number">10</span><span class="dica"><textarea>${personagem.dicaDez}</textarea></span></div>
+                                </div>
+                                <br>
+                        </div>	
+                        </div>
+                                <span class="btn proximo mb-5">Atualizar</span>
+                        </div>`;
+
+                        this.editarPersonagens(personagem.id);
+
+                        // inputBusca.value = "";
+
+                        // return;
+                    }
+                })
+            })
+    }
+
+    editarPersonagens(id){
+        console.log(id)
+        let $btnAtualizar = document.querySelector('.proximo');
+        $btnAtualizar.addEventListener('click', ()=>{
+            let $inputsTextArea = document.querySelectorAll('textarea');
+            let dataArray = [];
+            $inputsTextArea.forEach(input => {
+                    if(input.value === ""){
+                        alert("Preencha todos os campos");
+                        return;
+                    }
+
+                    dataArray.push(input.value);               
+            })
+
+            let _dataArray = {"nivel": dataArray[0], "personagem": dataArray[1], "dicaUm": dataArray[2], "dicaDois": dataArray[3], "dicaTres": dataArray[4], "dicaQuatro": dataArray[5], "dicaCinco": dataArray[6], "dicaSeis": dataArray[7], "dicaSete": dataArray[8], "dicaOito": dataArray[9], "dicaNove": dataArray[10], "dicaDez": dataArray[11], "id": id};
+
+            this.jogoController.edit(_dataArray)
+                    .then(response => alert("Deu certo"));
+
+        });
     }
 }
